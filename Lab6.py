b44a20386afe18813d9b7a32fee8a918c28097c7
William Deo Remulla <>
2024-10-09 15:20:51 +0800

    fin

--- Lab6.py
+++ Lab6.py
@@ -3,15 +3,15 @@ member = input("Are you a member?: ")
 if age<18:
     if member==str("yes") :
         print("Fee: 450.00 Php")
-    else:
+    if member==str("no") :
         print("Fee: 650.00 Php")
 if age>=18 and age<=65:
     if member==str("yes") :
         print("Fee: 550.00 Php")
-    else:
+    if member==str("no") :
         print("Fee: 750 Php")
 if age>65:
     if member==str("yes") :
         print("Fee: 400 Php")
-    else:
+    if member==str("no") :
         print("Fee: 600 Php")
